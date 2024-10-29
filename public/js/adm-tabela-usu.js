d83de79bf0fee9b2f5f02faf25ae2cc8032e2e38
luizamc003 <luizamachadocn@outlook.com>
2024-10-29 19:16:37 -0300

    fix buttons

--- public/js/adm-tabela-usu.js
+++ public/js/adm-tabela-usu.js
@@ -1,9 +1,22 @@
 function openModal(modalId) {
-    const modal = document.getElementById(modalId)
-    modal.style.display = 'block'
+  const modal = document.getElementById(modalId);
+  modal.style.display = "block";
 }
 
 function closeModal(modalId) {
-    const modal = document.getElementById(modalId)
-    modal.style.display = 'none'
+  const modal = document.getElementById(modalId);
+  modal.style.display = "none";
 }
+
+// Adiciona um evento de teclado para fechar o modal com a tecla ESC
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Escape") {
+    //comparando tecla
+    const modals = document.querySelectorAll(".modal"); //seleciona tudo com .modal
+    modals.forEach((modal) => {
+      if (modal.style.display === "block") {
+        modal.style.display = "none";
+      }
+    });
+  }
+});
