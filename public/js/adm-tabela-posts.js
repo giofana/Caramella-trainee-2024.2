b7800f653e8a6034619d8fde82a22fb600cc7fdf
BrunoCesarioMenezes <brunoagjf@gmail.com>
2024-10-29 04:02:14 -0300

    adiciona logica de abertura e fechamento de modal

---
+++ public/js/adm-tabela-posts.js
@@ -0,0 +1,11 @@
+
+
+function openModal(modalId) {
+    const modal = document.getElementById(modalId)
+    modal.style.display = 'block'
+}
+
+function closeModal(modalId) {
+    const modal = document.getElementById(modalId)
+    modal.style.display = 'none'
+}
