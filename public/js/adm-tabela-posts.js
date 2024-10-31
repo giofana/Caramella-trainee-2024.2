a8d7fd26f192d9f4f619f4d418dc70544f509eda
BrunoCesarioMenezes <brunoagjf@gmail.com>
2024-10-31 04:40:14 -0300

    .

--- public/js/adm-tabela-posts.js
+++ public/js/adm-tabela-posts.js
@@ -2,12 +2,12 @@ const tela = document.querySelector('#tela');
 
 function openModal(modalId) {
     const modal = document.getElementById(modalId)
-    modal.style.display = 'block'
+    modal.style.display = 'block';
     tela.style.display = 'block';
 }
 
 function closeModal(modalId) {
     const modal = document.getElementById(modalId)
-    modal.style.display = 'none'
+    modal.style.display = 'none';
     tela.style.display = 'none';
 }
