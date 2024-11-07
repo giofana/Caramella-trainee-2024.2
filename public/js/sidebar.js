73a940b52d7da796cb61c6dee0276aaaf9a9b889
luizamc003 <luizamachadocn@outlook.com>
2024-11-07 10:31:26 -0300

    fix icon inicial

--- public/js/sidebar.js
+++ public/js/sidebar.js
@@ -1,10 +1,9 @@
-document.getElementById("open-button").addEventListener("click", function () {
+document.addEventListener("DOMContentLoaded", function () {
   const sidebar = document.getElementById("sidebar");
-  sidebar.classList.toggle("close");
-
   const openIcon = document.getElementById("open-button-icon");
   const closeIcon = document.getElementById("close-button-icon");
 
+  // Define a visibilidade inicial dos ícones com base no estado da barra lateral
   if (sidebar.classList.contains("close")) {
     openIcon.style.display = "block";
     closeIcon.style.display = "none";
@@ -12,4 +11,16 @@ document.getElementById("open-button").addEventListener("click", function () {
     openIcon.style.display = "none";
     closeIcon.style.display = "block";
   }
+
+  document.getElementById("open-button").addEventListener("click", function () {
+    sidebar.classList.toggle("close");
+
+    if (sidebar.classList.contains("close")) {
+      openIcon.style.display = "block";
+      closeIcon.style.display = "none";
+    } else {
+      openIcon.style.display = "none";
+      closeIcon.style.display = "block";
+    }
+  });
 });
