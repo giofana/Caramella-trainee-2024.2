ffe5f756abda44ba96090cf725670c9739a1e512
luizamc003 <luizamachadocn@outlook.com>
2024-11-07 10:26:16 -0300

    altera icon sidebar

--- public/js/sidebar.js
+++ public/js/sidebar.js
@@ -1,3 +1,15 @@
 document.getElementById("open-button").addEventListener("click", function () {
-  document.getElementById("sidebar").classList.toggle("close");
+  const sidebar = document.getElementById("sidebar");
+  sidebar.classList.toggle("close");
+
+  const openIcon = document.getElementById("open-button-icon");
+  const closeIcon = document.getElementById("close-button-icon");
+
+  if (sidebar.classList.contains("close")) {
+    openIcon.style.display = "block";
+    closeIcon.style.display = "none";
+  } else {
+    openIcon.style.display = "none";
+    closeIcon.style.display = "block";
+  }
 });
