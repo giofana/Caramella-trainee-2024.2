683247a225ef91eba929e54a3a75536f3ad173a1
giovana <giovana.maieli@codejr.com.br>
2024-11-11 10:15:26 -0300

    outro commit

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
 return [
     'database' => [
-        'name' => 'sexta_marcha',
+        'name' => 'caramella',
         'username' => 'root',
         'password' => '',
         'connection' => 'mysql:host=127.0.0.1',
