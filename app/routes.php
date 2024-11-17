796a7de923e3ff208aa6d56360595311ff1cf254
BrunoCesarioMenezes <brunoagjf@gmail.com>
2024-11-17 03:48:08 -0300

    cria rotas update e delete

--- app/routes.php
+++ app/routes.php
@@ -4,6 +4,8 @@ namespace App\Controllers;
 use App\Controllers\ExampleController;
 use App\Core\Router;
 
-    $router->get('', 'ExampleController@index');
+    $router->get('posts-list', 'PostsController@index');
+    $router->post('posts-list/edit-post', 'PostsController@edit');
+    $router->post('posts-list/delete', 'PostsController@delete');
 
 ?>
