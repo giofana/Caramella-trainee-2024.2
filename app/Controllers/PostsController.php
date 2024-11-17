91832cf829b6c4fc7dbc83ffbb03452721882d73
BrunoCesarioMenezes <brunoagjf@gmail.com>
2024-11-17 03:59:29 -0300

    implementa PostsController com update e delete

---
+++ app/Controllers/PostsController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Core\App;
+use Exception;
+
+class PostsController
+{
+
+    public function index()
+    {
+        $posts = App::get('database')->selectAll('posts');
+        return view('admin/posts-page', compact('posts'));
+    }
+
+    public function edit(){
+        $parameters = [
+            'title' => $_POST['editTitulo'],
+            'author' => $_POST['editAutor'],
+            'cost' => $_POST['editCusto'],
+            'difficulty' => $_POST ['editDificuldade'],
+            'time' => $_POST['editTempo'],
+            'history' => $_POST['editHistoria'],
+            'prepare' => $_POST['editPreparo'],
+            'image' => $_POST['editImagem'],
+            //'ingredients' => $_POST['editIngrediente'],
+        ];
+        $id = $_POST['editId'];
+
+        App::get('database')->update('posts', $id, $parameters);
+        header('Location: /posts-list');
+    }
+
+    public function delete(){
+        $id = $_POST['idDelete'];
+
+        App::get('database')->delete('posts', $id);
+        header('Location: /posts-list');
+    }
+
+}
+
+?>
