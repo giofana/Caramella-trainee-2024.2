c340b3a9459ad3143628f5411585c025ae0f60d0
BrunoCesarioMenezes <brunoagjf@gmail.com>
2024-11-23 02:50:26 -0300

    implementa funções para tratamento do upload de imagens

--- core/database/QueryBuilder.php
+++ core/database/QueryBuilder.php
@@ -29,6 +29,62 @@ class QueryBuilder
         }
     }
 
+
+    public function verificaErroUpload($arquivo){
+        switch ($arquivo['error']){
+            case 1:
+                echo "Arquivo excede o tamanho máximo permitido.";
+                die();
+            case 2:
+                echo "Arquivo excede tamanho máximo permitido no formulário.";
+                die();
+            case 3:
+                echo "Upload não concluído.";
+                die();
+        }
+    }
+
+
+    public function verificaExtensao($ext){
+        if($ext === "jpg" || $ext === "jpeg" || $ext === "png"){
+            return;
+        }
+        echo "Arquivo não é uma imagem / Extensão não permitida.";
+        die();
+    }
+
+
+    public function uploadImage ($arquivo, $id){
+
+        $pasta = "public/imagens/";
+        $nomeArquivo = $arquivo['name'];
+
+        try{
+
+            if ($nomeArquivo != "" || $id==0){
+                $ext = strtolower(pathinfo($nomeArquivo, PATHINFO_EXTENSION));
+                $nomeArquivo = uniqid();
+                move_uploaded_file($arquivo['tmp_name'], $pasta . $nomeArquivo . "." . $ext);
+                return $pasta . $nomeArquivo . "." . $ext;
+            }
+
+            else{
+                $sql = sprintf ('SELECT %s FROM %s WHERE id=%s',"image","posts", $id);
+                $stmt = $this->pdo->prepare($sql);
+                $stmt->execute();
+                $post = $stmt -> fetchAll(PDO::FETCH_ASSOC);
+                return $post[0]['image'];
+            }
+
+        }
+
+        catch (Exception $e){
+            die($e->getMessage());
+        }
+    }
+
+
+
     public function update($table, $id, $parameters)
     {
         $sql = sprintf ('UPDATE %s SET %s WHERE id = %s',
@@ -47,7 +103,7 @@ class QueryBuilder
     } catch (Exception $e) {
         die($e->getMessage());
     }
-    
+
     }
 
     public function delete($table, $id){
@@ -71,8 +127,8 @@ class QueryBuilder
     public function insert($table, $parameters){
         $sql = sprintf('INSERT INTO %s (%s) VALUES (%s)',
         $table,
-        implode(', ', array_keys($parameters)), 
-        ':' . 
+        implode(', ', array_keys($parameters)),
+        ':' .
         implode(', :', array_keys($parameters))
     );
 
