746bd95aa982c7199ac3d0a5cb019a3ea1dcc5b1
JG-Souza <joaogsouzafma@gmail.com>
2024-12-07 18:20:50 -0300

    tamanho dos modais

--- public/js/adm-tabela-usu.js
+++ public/js/adm-tabela-usu.js
@@ -1,22 +1,21 @@
 function openModal(modalId) {
-    const modal = document.getElementById(modalId);
-    if (modal) {
-      modal.style.display = "block";
-      console.log('achei o id')
-    } else {
-      console.log(`Modal with ID '${modalId}' not found.`);
-    }
+  const modal = document.getElementById(modalId);
+  if (modal) {
+    modal.style.display = "block";
+    console.log("achei o id");
+  } else {
+    console.log(`Modal with ID '${modalId}' not found.`);
   }
-  
-  function closeModal(modalId) {
-    const modal = document.getElementById(modalId);
-    if (modal) {
-      modal.style.display = "none";
-    } else {
-      console.warn(`Modal with ID '${modalId}' not found.`);
-    }
+}
+
+function closeModal(modalId) {
+  const modal = document.getElementById(modalId);
+  if (modal) {
+    modal.style.display = "none";
+  } else {
+    console.warn(`Modal with ID '${modalId}' not found.`);
   }
-  
+}
 
 // Adiciona um evento de teclado para fechar o modal com a tecla ESC
 document.addEventListener("keydown", function (event) {
@@ -31,22 +30,22 @@ document.addEventListener("keydown", function (event) {
   }
 });
 
-  const senhaInput = document.getElementById("senhaInput");
-  const viewSenha = document.getElementById("viewSenha");
-
+document.querySelectorAll(".modalView").forEach((modal) => {
+  const senhaInput = modal.querySelector("input.senha");
+  const viewSenha = modal.querySelector(".view1 img");
 
   viewSenha.addEventListener("mousedown", () => {
-    senhaInput.type = "text"; 
-    console.log("Hello World")
+    senhaInput.type = "text";
+    console.log("Hello World");
   });
 
-
   viewSenha.addEventListener("mouseup", () => {
-    senhaInput.type = "password"; 
-    console.log("World")
+    senhaInput.type = "password";
+    console.log("World");
   });
 
   viewSenha.addEventListener("mouseleave", () => {
-    senhaInput.type = "password"; 
-    console.log("Hello")
+    senhaInput.type = "password";
+    console.log("Hello");
   });
+});
