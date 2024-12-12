82285ff9fdbe89feca64fd8e05dd601d15d4bff9
G-Braz <guilhermelordmud@gmail.com>
2024-12-12 17:44:57 -0300

    fazendo o secret

--- app/views/site/secret.view.php
+++ app/views/site/secret.view.php
@@ -15,27 +15,28 @@
   </head>
   <body>
 
+  <?php include "navbar.php"; ?>
+
+  <div class = "tronco">
     <!-- Apresentaçao da pagina  -->
     <div class = "aprese">
-      <h1>Você acaba de descobrir o segredo mais doce da Caramella</h1>
-      <p>A receita do cookie perfeito agora esta a poucos passos de você!</p>
-    </div>
-    <!-- Fim da apresentaçao da pagina  -->
-    <!-- De fato o melhor cookie  -->
-    <div class = "wow">
-     <h1>Prepare-se para criar o melhor cookie da sua vida!</h1>
+      <div class = "apre">
+        <h1>Você acaba de descobrir o segredo mais doce da Caramella</h1>
+        <p>A receita do cookie perfeito agora esta a poucos passos de você!</p>
+      </div>
+      <div class = "ap"><h1>Prepare-se para criar o melhor cookie da sua vida!</h1></div>
     </div>
     <!-- Fim da div mas de fato o melhor cookie  -->
     <!-- Ingredientes  -->
     <div class = "ingreb">
       <h2>Ingredientes</h2>
         <ul>
-          <li>100g de Manteiga ou Margarina</li>
+          <li>200g de Manteiga ou Margarina</li>
           <li>100g de Açúcar</li>
-          <li>100g de Açúcar Mascavo</li>
+          <li>120g de Açúcar Mascavo</li>
           <li>1 Ovo</li>
-          <li>250g de Farinha</li>
-          <li>1/2 colher (chá) de Fermento</li>
+          <li>350g de Farinha</li>
+          <li>1/2 colher (sobremesa) de Fermento</li>
           <li>2 Barras de Chocolate</li>
         </ul>
     </div>
@@ -46,14 +47,13 @@
         <ol>
           <li>Em uma tigela, misture bem a margarina com os açúcares até a mistura ficar mais clara e cremosa.</li>
           <li>Adicione o ovo e mexa até incorporar totalmente à massa.</li>
-          <li>Em outro recipiente, misture a farinha e o fermento e peneire essa mistura sobre a massa preparada.</li>
-          <li>Junte as barras de chocolate e reserve uma parte para decorar os cookies depois.</li>
+          <li>Em outra tigela separada, misture a farinha e o fermento e peneire essa mistura sobre a massa preparada.</li>
+          <li>Pique as barras de chocolate e reserve uma parte para decorar os cookies depois.</li>
           <li>Misture o chocolate picado à massa até que fique bem distribuído.</li>
-          <li>Modele os cookies e disponha-os em uma forma forrada com papel manteiga, deixando espaço entre eles.</li>
-          <li>Leve a forma ao freezer por 30 minutos.</li>
+          <li>Modele bolinhas com a massa no tamanho desejado, coloque-as em uma assadeira, decore, e leve ao freezer por 12 minutos ou à geladeira por 30 minutos.</li>
           <li>Preaqueça o forno a 200°C por 10 minutos.</li>
-          <li>Asse os cookies no forno por 10 minutos.</li>
-          <li>Após retirar os cookies do forno, deixe esfriar por 5 minutos antes de servir.</li>
+          <li>Coloque os cookies no forno por 8 minutos.</li>
+          <li>Após reduza a temperatura do forno para 180°C e asse por mais 8 minutos até os cookies ficarem levemente dourados.</li>
         </ol>
     </div>
     <!-- Fim modo de preparo  -->
@@ -61,13 +61,18 @@
     <div class = "dicks">
       <h1>Dicas</h1>
         <ul>
-          <li>Se preferir cookies mais crocantes, deixe-os na forma por mais tempo, até que fiquem bem dourados por dentro.</li>
+          <li>Se preferir cookies mais crocantes, deixe-os no forno por mais tempo, até que fiquem bem dourados por dentro.</li>
           <li>Deixe espaço suficiente entre os cookies na assadeira, pois eles se espalham ao assar.</li>
           <li>Armazene os cookies em um pote fechado para mantê-los frescos por mais tempo.</li>
-          <li>Para um sabor extra, adicione 1 colher de chá de essência de baunilha à massa antes de assar.</li>
+          <li>Para um sabor extra, adicione 1 colher de chá de essência de baunilha à massa junto com o ovo.</li>
           <li>Os cookies podem queimar rapidamente, então observe com atenção enquanto assam. Lembre-se de que cada forno é diferente e os tempos podem variar.</li>
         </ul>
     </div>
     <!-- Fim das Dicks -->
+    </div>
+
+    <?php include 'footer.view.php';
+ ?>
+
   </body>
 </html>
